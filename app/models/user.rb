abfd20553ea18dd3617389070cf73d944c4853a9
jlbenavente <jlbenaventecanales@gmail.com>
2019-02-08 20:09:51 -0300

    3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	has_many :assignments
+	has_many :tasks, through: :assignments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_225947) do
+ActiveRecord::Schema.define(version: 2019_02_08_230439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "task_id"
+    t.boolean "status"
+    t.datetime "completed_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_assignments_on_task_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -32,4 +43,6 @@ ActiveRecord::Schema.define(version: 2019_02_08_225947) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "assignments", "tasks"
+  add_foreign_key "assignments", "users"
 end
